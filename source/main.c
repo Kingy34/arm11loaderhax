d0708894a29bdbe0061e767fa7d493436765f7e2
Kingy <thekingy35@gmail.com>
2016-12-16 19:55:45 +0000

    Forgot to update the fucking main.c
    fml

--- source/main.c
+++ source/main.c
@@ -1,29 +1,70 @@
 #include <string.h>
 #include <3ds.h>
+#include <stdio.h>
+
+void cls() {
+	printf("\e[1;1H\e[2J");
+}
 
 int main()
 {
 	gfxInitDefault();
-	//gfxSet3D(true); //Uncomment if using stereoscopic 3D
-	consoleInit(GFX_TOP, NULL) //Change this line to consoleInit(GFX_BOTTOM, NULL) if using the bottom screen.
+	consoleInit(GFX_TOP, NULL)
+	aptInit();
+    fsInit();
+	
+	     
+	printf("\x1b[32mExecuting Kernel Hax...\x1b[0m\n");
+	svcSleepThread(2000000000);
+	
+    cls();
+	
 
+	    printf("Arm11loaderhax installer\n");
+		printf("Version 1.0-45429A\n");
+		printf("Created by Kingy\n\n\n");
+		printf("Press A to install!");
+	
 	// Main loop
 	while (aptMainLoop())
 	{
 		gspWaitForVBlank();
 		hidScanInput();
-
-		//Your code goes here
-
 		u32 kDown = hidKeysDown();
+
+				if (kDown & KEY_A) {
+			printf("Retrieving System Infomation...\n");
+            svcSleepThread(5000000000);
+			
+			printf("Backing up kernel...\n");
+            svcSleepThread(10000000000);
+			
+			printf("Done, Accessing servers...\n");
+			svcSleepThread(5000000000);
+			
+			printf("Installing kernel patches...\n\n");
+			svcSleepThread(200000000);
+			printf("Installing backdoor payload... (1/3)\n");
+			svcSleepThread(5000000000);
+			printf("Patching kernel execution... (2/3)\n"); 
+			svcSleepThread(10000000000);
+			printf("\x1b[31mERROR: \x1b[0mCould not repair kernel damages, rebooting...\n");
+			svcSleepThread(800000000);
+			cls();
+			
+				}
+			
 		if (kDown & KEY_START)
-			break; //Break in order to return to hbmenu
+			break;
 
 		// Flush and swap frame-buffers
 		gfxFlushBuffers();
 		gfxSwapBuffers();
+		gspWaitForVBlank
 	}
 
 	gfxExit();
+	aptExit();
+	fsExit();
 	return 0;
 }
